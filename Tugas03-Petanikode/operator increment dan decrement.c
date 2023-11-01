ad6e84ed1a3a4dd32a2b36220c5cfe856504a7e5
ardeliaelvv <ardeliaelvandarii@gmail.com>
2023-11-01 11:07:14 +0700

    Const.cfungsi fgets.cfungsi gets.cinput nilai.cleft shift.coperasi pembanding.c

---
+++ Tugas03-Petanikode/operator increment dan decrement.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main(){
+    int a = 4;
+
+    // increment a
+    a++;
+
+    printf("a++ = %d\n", a);
+
+    // increment a
+    ++a;
+
+    printf("++a = %d\n", a);
+
+    // decrement a
+    a--;
+
+    printf("a-- = %d\n", a);
+
+    // decrement a
+    --a;
+
+    printf("--a = %d\n", a);
